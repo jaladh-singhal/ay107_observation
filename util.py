bcfdb6fc7bd6c45ab3d9548e4c0d4653604f9444
dennisraush <dennis@waveye.com>
2024-05-24 00:27:41 -0700

    starting on wavelength solution

--- util.py
+++ util.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 # Plotting ------------------------------------------------------------
 
-def clim(data, prange=(1,99)):
+def clim(data, prange=(1, 99)):
     return (np.percentile(data, prange[0]), np.percentile(data, prange[1]))
 
 
-def plot_frame(frame, figsize=(15,3), aspect=None, prange=(1,99), label=None, title=None):
+def plot_frame(frame, figsize=(15, 3), aspect=None, prange=(1, 99), label=None, title=None):
     plt.figure(figsize=figsize)
     plt.imshow(frame, origin='lower', clim=clim(frame, prange), aspect=aspect)
     plt.colorbar(label=label)
@@ -16,11 +16,21 @@ def plot_frame(frame, figsize=(15,3), aspect=None, prange=(1,99), label=None, ti
         plt.title(title)
 
 
+def plot_frame2(frame, figsize=(15, 3), aspect=None, prange=(1, 99), label=None, title=None):
+    print('returning')
+    fig, ax = plt.subplots(figsize=figsize)
+    h = ax.imshow(frame, origin='lower', clim=clim(frame, prange), aspect=aspect)
+    plt.colorbar(h, label=label)
+    if title:
+        ax.set_title(title)
+    return fig, ax
+
 
 # Data transformation ---------------------------------------------------
 
 def reduce_frame(frame, exptime, master_flat, master_bias):
-    return ((frame - master_bias)/exptime) / master_flat #subtract dark too?
+    return ((frame - master_bias) / exptime) / master_flat  # subtract dark too?
+
 
 def correct_airmass(star_wvl, star_frame, extinction_wvl, extinction, airmass):
     airmass_extinction = extinction * airmass
@@ -32,17 +42,20 @@ def correct_airmass(star_wvl, star_frame, extinction_wvl, extinction, airmass):
 
 
 def extract_source_spectrum(science_data, aperture_idx_range):
-    return np.sum(science_data[aperture_idx_range[0] : aperture_idx_range[1]+1], axis=0)
+    return np.sum(science_data[aperture_idx_range[0]: aperture_idx_range[1] + 1], axis=0)
+
 
 def extract_bg_spectrum(science_data, bg_lower_idx, bg_upper_idx):
     return np.median(np.concatenate((
-        science_data[bg_lower_idx[0]:bg_lower_idx[1]+1],
-        science_data[bg_upper_idx[0]:bg_upper_idx[1]+1]),
+        science_data[bg_lower_idx[0]:bg_lower_idx[1] + 1],
+        science_data[bg_upper_idx[0]:bg_upper_idx[1] + 1]),
         axis=0), axis=0)
 
+
 def spectrum_wo_bg(source_spectrum, aperture_idx_range, bg_spectrum):
-    aperture_nrows = aperture_idx_range[1] - aperture_idx_range[0]+1
+    aperture_nrows = aperture_idx_range[1] - aperture_idx_range[0] + 1
     return source_spectrum - (bg_spectrum * aperture_nrows)
 
+
 def gaussian(x, h, mu, sigma):
-    return h * np.exp(-((x - mu)**2) / (2 * sigma**2))
+    return h * np.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
